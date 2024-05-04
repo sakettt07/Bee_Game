8c7b780646ae410bba29f69484ce0aa25e9565a7
saket <saket@saketgupta.com>
2024-05-04 14:44:41 +0530

    A new react+gsap project started

---
+++ Bee-Game/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Loading from './components/Loading';
+import "./App.css"
+const App = () => {
+  return (
+    <div>
+      <Loading />
+    </div>
+  )
+}
+
+export default App
---
+++ Bee-Game/src/components/GameScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GameScreen = () => {
+  return (
+    <div>
+      <h2>This is my game screen</h2>
+    </div>
+  )
+}
+
+export default GameScreen
