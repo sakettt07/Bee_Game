b7194f234e9aa0b137e9821e1c87196f2b91a58d
saket <saket@saketgupta.com>
2024-05-06 23:35:29 +0530

    collecting ideas for the gamescreen

--- Bee-Game/src/components/GameScreen.jsx
+++ Bee-Game/src/components/GameScreen.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import {useGSAP} from "@gsap/react";
+import gsap from 'gsap';
 
 const GameScreen = () => {
+  const bref=useRef();
+  useGSAP(()=>{
+    gsap.from(".box",{
+      y:200,
+      scale:1.3,
+      yoyo:true,
+      opacity:0,
+      duration:1.9,
+      x:200
+    })
+  })
   return (
-    <div>
-      <h2>This is my game screen</h2>
+    <>
+    <div className='h-screen w-full bg-zinc-950'>
+
+      <h2 ref={bref} className='textt text-white text-[8vw]'>This is my game screen</h2>
+      <div className='box h-32 w-32 bg-yellow-400'></div>
     </div>
+    </>
   )
 }
 
